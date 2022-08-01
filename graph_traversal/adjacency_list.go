e3427c6c3795e6d514491870feb7d9dc3485e0bd
Chester Asuncion <asuncionmcv@gmail.com>
2022-08-01 20:11:55 +0800

    implement bfs

--- graph_traversal/adjacency_list.go
+++ graph_traversal/adjacency_list.go
@@ -52,7 +52,72 @@ func (g *graph) InsertEdge(x, y int) {
 }
 
 func (g *graph) Bfs(start int) {
+	// disregard parent
+	/*
+		discovered bool -> arr with size vertex
+		processed bool -> arr with size vertex
 
+		processing_que -> que of vertex to process
+
+		enque(processing_que, start)
+		discovered[start] = TRUE
+		while the processing_que is not empty	{
+			current_vertex = deque(processing_que)
+			adjacency_list_of_current_vertex = edges[current_vertex]
+			while adjacency_list_of_current_vertex != null {
+				// check status of successor vertex and act accordingly
+				if !discovered[successor_vertex] {
+					// discover successor_vertex
+					discovered[successor_vertex] = true
+					enque(processing_que, successor_vertex)
+				}
+				// ?? don't know this step
+				if !processed[successor_vertex] || g.directed {
+					process_edge(v, y)
+				}
+				// go to next
+				adjacency_list = adjacency_list.next
+			}
+		}
+		// set current_vertex as processed
+		proccessed[current_vertex] = TRUE
+	*/
+
+	var v int       // current vertex
+	var y int       // successor vertex
+	var p *edgenode // adjacency list of the current vertex
+
+	discovered := make([]bool, g.nvertices)
+	processed := make([]bool, g.nvertices)
+
+	processing_que := make([]int, 0)
+	// enque start
+	processing_que = append(processing_que, start)
+	discovered[start] = true
+
+	for len(processing_que) != 0 {
+		// deque first item in processing que
+		v = processing_que[0]
+		processing_que = processing_que[1:]
+
+		// get the adjacency list of the current vertex (v)
+		p = g.edges[v]
+		for p != nil {
+			y = p.y
+			if !discovered[y] {
+				discovered[y] = true
+				processing_que = append(processing_que, y)
+			}
+			/*
+				if !processed[y] || g.directed {
+					// do something
+				}
+			*/
+			p = p.next
+		}
+
+		processed[v] = true
+	}
 }
 
 func (g *graph) Print() {
